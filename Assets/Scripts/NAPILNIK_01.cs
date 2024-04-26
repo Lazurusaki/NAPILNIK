d5d32dcccf09243ead544b67249257b9f5301635
Lazurusaki <archines93@gmail.com>
2024-04-26 23:23:05 +0300

    edit

--- Assets/Scripts/NAPILNIK_01.cs
+++ Assets/Scripts/NAPILNIK_01.cs
@@ -7,8 +7,19 @@ public class NAPILNIK_01
         private int _damage;
         private int _bullets;
 
+        public Weapon(int damage, int bullets)
+        {
+            _damage = Mathf.Max(0,damage);
+            _bullets = Mathf.Max(0, bullets);
+        }
+
         public void Fire(Player player)
         {
+            if (player == null)
+            {
+                return;
+            }
+
             if (_bullets > 0)
             {
                 player.TakeDamage(_damage);
@@ -20,11 +31,20 @@ public class NAPILNIK_01
     class Player
     {
         private int _health;
-        private int _minHealth = 0;
+
+        public Player(int health)
+        {
+            _health = Mathf.Max(0, health);
+        }
 
         public void TakeDamage(int damage)
         {
-            _health = Mathf.Max(_minHealth, _health - damage);
+            if (damage <= 0)
+            {
+                return;
+            }
+
+            _health = Mathf.Max(0, _health - damage);
         }
     }
 
@@ -32,8 +52,18 @@ public class NAPILNIK_01
     {
         private Weapon _weapon;
 
+        public Bot(Weapon weapon) 
+        {
+            _weapon = weapon;
+        }
+
         public void OnSeePlayer(Player player)
         {
+            if (player == null && _weapon == null)
+            {
+                return;
+            }
+
             _weapon.Fire(player);
         }
     }
