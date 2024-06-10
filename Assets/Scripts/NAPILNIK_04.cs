e5011c631b7926567361528150cbbe8c099e3d04
Lazurusaki <archines93@gmail.com>
2024-06-10 11:31:41 +0300

    Update NAPILNIK_04.cs

--- Assets/Scripts/NAPILNIK_04.cs
+++ Assets/Scripts/NAPILNIK_04.cs
@@ -9,120 +9,162 @@ namespace NAPILNIK_04
         public string GetPayingLink(Order order);
     }
 
+    public interface IHasher
+    {
+        public string GetHash(string input);
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             string secretKey = "UltraMegaSuperSecretSystemKey";
 
-            System1 system1 = new System1();
-            System2 system2 = new System2();
-            System3 system3 = new System3(secretKey);
-
             int orderID = 213214;
-            int orderAmount  = 100000;
+            int orderAmount = 100000;
 
             Order order = new Order(orderID, orderAmount);
 
+            string system1Base = "pay.system1.ru/order?amount=" + order.Amount + "RUB&hash=";
+            string system2Base = "order.system2.ru/pay?hash=";
+            string system3Base = "system3.com/pay?amount=" + order.Amount + "&currency=RUB&hash=";
+
+            System1 system1 = new System1(system1Base, new MD5Hasher());
+            System2 system2 = new System2(system2Base, new MD5Hasher());
+            System3 system3 = new System3(system3Base, secretKey, new SHA1Hasher());
+  
             system1.GetPayingLink(order);
             system2.GetPayingLink(order);
             system3.GetPayingLink(order);
         }
     }
 
-    public static class HashUtility
+    public class MD5Hasher : IHasher
     {
-        public static string GetMd5Hash(string input)
+        public string GetHash(string input)
         {
-            if (input == null) throw new ArgumentNullException("Input string can't be null");
-            if (input.Length < 1) throw new ArgumentException("Input string can't be empty");
+            if (input == null) 
+                throw new ArgumentNullException("Input string can't be null");
+
+            if (input.Length < 1)
+                throw new ArgumentException("Input string can't be empty");
 
             using (MD5 md5 = MD5.Create())
             {
                 byte[] inputBytes = Encoding.ASCII.GetBytes(input);
                 byte[] hashBytes = md5.ComputeHash(inputBytes);
-                StringBuilder sb = new StringBuilder();
+                StringBuilder stringBuiler = new StringBuilder();
+
                 for (int i = 0; i < hashBytes.Length; i++)
                 {
-                    sb.Append(hashBytes[i].ToString("X2"));
+                    stringBuiler.Append(hashBytes[i].ToString("X2"));
                 }
-                return sb.ToString();
+
+                return stringBuiler.ToString();
             }
         }
+    }
 
-        public static string GetSha1Hash(string input)
+    public class SHA1Hasher : IHasher
+    {
+        public string GetHash(string input)
         {
-            if (input == null) throw new ArgumentNullException("Input string can't be null");
-            if (input.Length < 1) throw new ArgumentException("Input string can't be empty");
+            if (input == null) 
+                throw new ArgumentNullException("Input string can't be null");
+
+            if (input.Length < 1) 
+                throw new ArgumentException("Input string can't be empty");
 
             using (SHA1 sha1 = SHA1.Create())
             {
                 byte[] inputBytes = Encoding.ASCII.GetBytes(input);
                 byte[] hashBytes = sha1.ComputeHash(inputBytes);
-                StringBuilder sb = new StringBuilder();
+                StringBuilder stringBuiler = new StringBuilder();
+
                 for (int i = 0; i < hashBytes.Length; i++)
                 {
-                    sb.Append(hashBytes[i].ToString("X2"));
+                    stringBuiler.Append(hashBytes[i].ToString("X2"));
                 }
-                return sb.ToString();
+
+                return stringBuiler.ToString();
             }
         }
     }
 
-
     public class Order
     {
-        private int _id;
-        private int _amount;
-
         public int ID { get; private set; }
         public int Amount { get; private set; }
 
         public Order(int id, int amount)
         {
-            if (!int.TryParse(id.ToString(), out _)) throw new ArgumentException("Incorrect ID");
-            if (amount < 1) throw new ArgumentOutOfRangeException("Amount can't be less than 1");
+            if (int.TryParse(id.ToString(), out _) == false) 
+                throw new ArgumentException("Incorrect ID");
 
-            _id = id;
-            _amount = amount;
+            if (amount < 1) 
+                throw new ArgumentOutOfRangeException("Amount can't be less than 1");
+
             ID = id;
             Amount = amount;
         }
     }
 
-    public class System1 : IPaymentSystem
+    public class System1: IPaymentSystem
     {
-        public string GetPayingLink(Order order)
+        protected string _linkBase;
+        protected IHasher _hasher;
+
+        public System1(string linkBase, IHasher hasher)
         {
-            string link = "hash=" + HashUtility.GetMd5Hash(order.ID.ToString());
-            return link;
+            if (linkBase == null)
+                throw new ArgumentNullException("LinkBase can't be null");
+
+            if (linkBase.Length < 1)
+                throw new ArgumentException("LinkBase can't be empty");
+
+            if (hasher == null)
+                throw new ArgumentNullException("Hasher can't be null");
+
+            _linkBase = linkBase;
+            _hasher = hasher;
+        }
+
+        public virtual string GetPayingLink(Order Order)
+        {
+            return _linkBase + _hasher.GetHash(Order.ID.ToString());
+        }    
+    }
+
+    public class System2 : System1
+    {
+        public System2(string linkBase, IHasher hasher) : base(linkBase, hasher)
+        {
+        }
+
+        public override string GetPayingLink(Order Order)
+        {
+            return _linkBase + _hasher.GetHash(Order.ID.ToString() + Order.Amount.ToString());
         }
     }
 
-    public class System2 : IPaymentSystem
-    {
-        public string GetPayingLink(Order order)
-        {
-            string link = "hash=" + HashUtility.GetMd5Hash(order.ID.ToString()) + "amount=" + order.Amount;
-            return link;
-        }
-    }
-
-    public class System3 : IPaymentSystem
+    public class System3 : System1
     {
         private string _key;
 
-        public System3(string key)
+        public System3(string linkBase, string key, IHasher hasher ) : base(linkBase, hasher)
         {
-            if (key.Length == 0) throw new ArgumentException("Secret key can't be empty");
+            if (key == null)
+                throw new ArgumentNullException("Secret key can't be null");
+
+            if (key.Length < 1)
+                throw new ArgumentException("Secret key can't be empty");
 
             _key = key;
         }
 
-        public string GetPayingLink(Order order)
+        public override string GetPayingLink(Order Order)
         {
-            string link = "hash=" + HashUtility.GetSha1Hash(order.Amount.ToString()) + order.ID +  "key=" + _key;
-            return link;
+            return _linkBase + _hasher.GetHash(Order.Amount.ToString() + Order.ID.ToString() + _key);
         }
     }
 }
